369b408cb802b73b9c2ef9b9ab8fffd477c892ed
Syed Hussain <syed.hussain@globalmobisoft.com>
2025-09-26 21:16:49 -0500

    Integrated Charity System

---
+++ Repositories/Interfaces/ICharityRepository.cs
@@ -0,0 +1,17 @@
+﻿// Repositories/Interfaces/ICharityRepository.cs
+using GMS.TifoXRCoreWebAPI.Models;
+
+namespace GMS.TifoXRCoreWebAPI.Repositories.Interfaces
+{
+    public interface ICharityRepository
+    {
+        Task<CharityData> CreateCharityAsync(CharityCreateDto dto);
+        Task<CharityData?> GetCharityByIdAsync(int id, int spaceId);
+        Task<List<CharityData>> GetCharitiesBySpaceAsync(int spaceId);
+
+        Task<CharityData> UpdateCharityAsync(int spaceId, int id, CharityUpdateDto dto);
+
+        Task<bool> DeleteCharityAsync(int spaceId, int id);
+
+    }
+}
